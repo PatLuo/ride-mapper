7e50fdf1d3ce7fc46c5b4eb4801e48e39f99c551
PatLuo <patrick.luong615@gmail.com>
2022-01-07 04:11:30 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import polyline from "@mapbox/polyline";
+import { MapContainer, TileLayer, LayersControl, Popup, Polyline, ZoomControl } from "react-leaflet";
+import "./app.css";
+import Sidebar from "./sidebar";
+
+const { BaseLayer } = LayersControl;
+
+function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const newTokenData = await axios.all([
+        axios.post(
+          `https://www.strava.com/oauth/token?client_id=${process.env.REACT_APP_CLIENTID}&client_secret=${process.env.REACT_APP_CLIENTSECRET}&refresh_token=${process.env.REACT_APP_REFRESHTOKEN}&grant_type=refresh_token`
+        ),
+      ]);
+      const accessToken = newTokenData[0].data.access_token;
+      const activityResponse = await axios.get(`https://www.strava.com/api/v3/athlete/activities?per_page=100&access_token=${accessToken}`);
+      const activityData = [];
+
+      for (let i = 0; i < activityResponse.data.length; i++) {
+        const data = activityResponse.data[i];
+        const startDate = new Date(data.start_date);
+        const activityDate = startDate.toLocaleDateString("en-US", { weekday: "long", year: "numeric", month: "long", day: "numeric" });
+        const activityPolyline = polyline.decode(data.map.summary_polyline);
+        const activityDuration = new Date(data.elapsed_time * 1000).toISOString().substr(11, 8);
+        const activityDistance = Math.round((data.distance / 1000) * 100) / 100;
+        activityData.push({ date: activityDate, polyline: activityPolyline, duration: activityDuration, distance: activityDistance });
+      }
+      setActivities(activityData);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className="app">
+      <MapContainer center={[45.3755676397901, -75.7501302762503]} zoom={13} scrollWheelZoom={true} zoomControl={false}>
+        <LayersControl>
+          <BaseLayer name="Light">
+            <TileLayer
+              attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+              url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+              maxZoom="20"
+            />
+          </BaseLayer>
+          <BaseLayer checked name="Dark">
+            <TileLayer
+              attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+              url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+              maxZoom="20"
+            />
+          </BaseLayer>
+          <BaseLayer name="Colorful">
+            <TileLayer
+              attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+              url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
+              maxZoom="20"
+            />
+          </BaseLayer>
+          <BaseLayer name="Satellite">
+            <TileLayer
+              attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            />
+          </BaseLayer>
+        </LayersControl>
+        <ZoomControl position="bottomright" />
+        <div className="shapes">
+          {activities.map((activity, i) => (
+            <Polyline className="polyline" key={i} positions={activity.polyline} pathOptions={{ color: "#54a9ff", stroke: "false" }}>
+              <Popup>
+                <h2>{activity.date}</h2>
+                <h3>Duration: {activity.duration}</h3>
+                <h3>Distance: {activity.distance} km</h3>
+              </Popup>
+            </Polyline>
+          ))}
+        </div>
+      </MapContainer>
+      <div className="background">
+        <div className="sidebar">
+          <Sidebar activities={activities} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/sidebar.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import "./sidebar.css";
+
+export default function Sidebar(props) {
+  const [numberOfRides, setNumberOfRides] = useState();
+  const [totalDistanceRounded, setTotalDistanceRounded] = useState();
+  const [highestOccurance, setHighestOccurance] = useState();
+  const [lastRide, setLastRide] = useState();
+
+  useEffect(() => {
+    const updateStats = () => {
+      const data = props.activities;
+
+      setNumberOfRides(data.length);
+
+      let totalDistance = 0;
+      for (const activity of data) {
+        totalDistance += activity.distance;
+      }
+      setTotalDistanceRounded(Math.round(totalDistance * 100) / 100);
+
+      let dayOccurance = [];
+      for (const activity of data) {
+        dayOccurance.push(activity.date.substr(0, activity.date.indexOf(",")));
+      }
+      const mode = (arr) =>
+        [...new Set(arr)]
+          .map((value) => [value, arr.filter((v) => v === value).length])
+          .sort((a, b) => a[1] - b[1])
+          .reverse()
+          .filter((value, i, a) => a.indexOf(value) === i)
+          .filter((v, i, a) => v[1] === a[0][1])
+          .map((v) => v[0]);
+      setHighestOccurance(mode(dayOccurance));
+      if (data.length !== 0) {
+        setLastRide(data[0].date);
+      }
+    };
+    updateStats();
+  }, [props.activities]);
+
+  return (
+    <div>
+      <div className="header">
+        <h1 className="title">Ride Mapper</h1>
+        <p>Mapping out all my bike rides across Ottawa🍁 using the Strava API. Made with react-leaflet.</p>
+        <h4>Click on a path for details</h4>
+      </div>
+      <hr className="headerDivider"></hr>
+      <div className="overview">
+        <h1>Overview</h1>
+        <div className="stat">
+          <h4>I've gone on...</h4>
+          <h1>{numberOfRides} rides</h1>
+        </div>
+        <hr className="statDivider"></hr>
+        <div className="stat">
+          <h4>Travelled a total of...</h4>
+          <h1>{totalDistanceRounded} km</h1>
+        </div>
+        <hr className="statDivider"></hr>
+        <div className="stat">
+          <h4>Most likely to go biking on a...</h4>
+          <h1>{highestOccurance}</h1>
+        </div>
+        <hr className="statDivider"></hr>
+        <div className="stat">
+          {" "}
+          <h4>My last bike ride was on...</h4>
+          <h1>{lastRide}</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
